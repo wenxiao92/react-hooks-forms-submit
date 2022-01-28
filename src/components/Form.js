984dd0aade01cf8bfa546394bc646f879e8fa97f
Wen Xiao <wennstyle@gmail.com>
2022-01-27 23:54:17 -0500

    commit tree e97582690cd012658b991e2184d6c406c1d570ca

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 function Form() {
   const [firstName, setFirstName] = useState("Sylvia");
   const [lastName, setLastName] = useState("Woods");
+  const [submittedData, setSubmittedData] = useState([]); //state to handle the form submission
+  const [errors, setErrors] = useState([]) //state to handle errors
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -12,12 +14,58 @@ function Form() {
     setLastName(event.target.value);
   }
 
+  function handleSubmit(event){
+    event.preventDefault()
+
+    if(firstName.length > 0){
+    const formData = { //object called formData where the keys are firstName and lastName with value from input
+      firstName: firstName,
+      lastName: lastName,
+    };
+    
+    const dataArray = [...submittedData, formData] //state where submittedData is the current array and formData will push the latest object into the array
+    setSubmittedData(dataArray); //accesses the state where the stores the value into the previous array
+    setFirstName("") //form resets where first name is set to blank
+    setLastName("") //form resets where last name is set to blank
+    setErrors([])
+  } else {
+    setErrors(["First name is required"])
+  }
+}
+
+  //used to display the names onto the webpage
+  const listOfSubmissions = submittedData.map((data, index) =>{
+    return(
+      <div key={index}>
+        {data.firstName} {data.lastName}
+        </div>
+    )
+  })
+
+
+
   return (
-    <form>
+    <div>
+    <form onSubmit={handleSubmit}>
       <input type="text" onChange={handleFirstNameChange} value={firstName} />
       <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
     </form>
+    
+    {/* Where the error message will display (if there is one). Error message will render after clicking submit */}
+    {errors.length > 0
+      ? errors.map((error, index) => (
+          <p key={index} style={{ color: "red" }}>
+            {error}
+          </p>
+        ))
+      : null}
+
+
+    {/* Where we display the submitted names */}
+    <h3>Submissions</h3>
+    {listOfSubmissions}
+    </div>
   );
 }
 
